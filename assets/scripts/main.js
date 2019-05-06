5dd70f0c94b5cda8d1a04b7c043c714b66614558
Ahenkan <akwaahenkan@gmail.com>
2019-05-06 17:20:08 +0000

    add fadeOut for contacts list

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,12 @@
   let add = document.getElementById('add');
   add.addEventListener('click', function(e){
       e.preventDefault();
+      e.target.parentElement.parentElement.classList.remove('fadeIn', 'delay-1s');
+      e.target.parentElement.parentElement.classList.add('fadeOutLeft');
+      console.log('classlist: ', e.target.parentElement.parentElement.classList);
 
-      alert('add somefin u modafucker');
+      let addContacts = document.querySelector('.add-contacts');
+      addContacts.classList.add('fadeInRight', 'delay-1s');
+     
+
   })
